fa0ea43fb8a96a5851982deeefdae1747e73c9de
Radon Rosborough <radon.neon@gmail.com>
2016-07-18 07:14:14 -0600

    Add MIT License, remove (c) notices

--- src/mazes/schematic/SchematicCellGrid.java
+++ src/mazes/schematic/SchematicCellGrid.java
@@ -1,6 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2014-2016 Radon Rosborough. All rights reserved.
- *******************************************************************************/
 package mazes.schematic;
 
 import mazes.util.DeepCopyable;
--- src/mazes/schematic/SchematicGenerator.java
+++ src/mazes/schematic/SchematicGenerator.java
@@ -1,6 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2014-2016 Radon Rosborough. All rights reserved.
- *******************************************************************************/
 package mazes.schematic;
 
 import java.text.SimpleDateFormat;
